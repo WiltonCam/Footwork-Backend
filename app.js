b6be268751f2943c6c4f0e2c19a2a54914736740
elizabethbright721 <elizabethbright721@gmail.com>
2023-05-09 10:54:58 -0400

    setup gitignore app.js server.js and package json files

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express") 
+const cors = require("cors");
+
+//CONFIGURATION
+const app = express();
+
+//MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+//ROUTES
+app.get("/", (req, res) => {
+    res.send("Welcome to the Footware App!")
+});
+
+module.exports = app;
